daa7d2db6b1709650107010fc00b0767fe235c00
Mitch Loar <mitchloarweb@gmail.com>
2016-08-31 09:08:02 -0600

    Assignment 7 update playSong positioning

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -5,38 +5,23 @@
      var currentSong = null;
      var currentBuzzObject = null;
 
-     
-     
-/**
- * @function setSong
- *@desc Stops currently from playing and loads new audio file as currentBuzzObject
- * @param {Object} song
- */
-     
-     
-     var setSong = function(song) {
+     /**
+      * @function setSong
+      *@desc Stops currently from playing and loads new audio file as currentBuzzObject
+      * @param {Object} song
+      */
+
+     var setSong = function (song) {
        if (currentBuzzObject) {
          currentBuzzObject.stop();
          currentSong.playing = null;
        }
-       
-/**
-* @function playSong
-* @desc Plays the the audio file loaded by setSong.
-* @param {Object} song
-*/
-       
-    var playSong = function(song) {
-     currentBuzzObject.play();
-      song.playing = true;
-      
-    };
-       
-     /**
- * @desc Buzz object audio file
- * @type {Object}
- */
-    
+
+       /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+
        currentBuzzObject = new buzz.sound(song.audioUrl, {
          formats: ['mp3'],
          preload: true
@@ -45,12 +30,23 @@
        currentSong = song;
      };
      
-     
+          
+       /**
+        * @function playSong
+        * @desc Plays the the audio file loaded by setSong.
+        * @param {Object} song
+        */
+       var playSong = function (song) {
+         currentBuzzObject.play();
+         song.playing = true;
+
+       };
+
+
      SongPlayer.play = function (song) {
        if (currentSong !== song) {
-
          setSong(song);
-         playSong; 
+         playSong();
 
        } else if (currentSong === song) {
          if (currentBuzzObject.isPaused()) {
